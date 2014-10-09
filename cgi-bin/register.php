621af91f3758f88be1e502315ade3ee44f4fdad1
jonprivate <jonprivate1991@hotmail.com>
2014-10-09 09:29:00 -0600

    add new changes to interact with database, but currently it does not work and need to be further debugged

--- cgi-bin/register.php
+++ cgi-bin/register.php
@@ -1,2 +1,43 @@
-Hi <?php echo htmlspecialchars($_POST['my_name']); ?>.
-You are <?php echo (int)$_POST['my_age']; ?> years old.
+<html>
+<body style="background-color: orange;">
+hello 0<br/>
+<!--Greeting the user-->
+	Hi <?php echo htmlspecialchars($_POST['username']); ?>.<br/>
+	Your email address: <?php echo htmlspecialchars($_POST['email']; ?><br/>
+hello 1<br/>
+
+<!--Interact with database-->
+	<?php
+	// access database and posted data
+	$db = new SQLite3('users.db') or die('Unable to open database');
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['email'];
+
+	// list database content before insertion
+	$result = $db->query('SELECT * FROM users') or die('Query failed');
+	echo "Username\t|\tEmail\t|\tPassword<br/>";
+	while($row = $result->fetchArray())
+	{
+		echo "{$row['username']}\t|\t{$row['email']}\t|\t{$row['password']}<br/>";
+	}
+
+	// insert the new user
+	$query = <<<EOD
+	INSERT INTO users VALUES ('$username', '$email', '$password')
+EOD;
+	$db->exec($query) or die("Unable to add user $username");
+
+	// list database content after insertion
+	echo "<br/>";
+	echo "After insertion:<br/>";
+	echo "Username\t|\tEmail\t|\tPassword<br/>";
+	while($row = $result->fetchArray())
+	{
+		echo "{$row['username']}\t|\t{$row['email']}\t|\t{$row['password']}<br/>";
+	}
+	?>
+</body>
+</html>
+
+
