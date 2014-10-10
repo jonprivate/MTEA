c0058fb1e837be6acf20f0ec17e8a30aeddd7c73
jonprivate <jonprivate1991@hotmail.com>
2014-10-09 19:13:24 -0600

    this version can work basically

--- cgi-bin/register.php
+++ cgi-bin/register.php
@@ -1,39 +1,30 @@
 <html>
 <body style="background-color: orange;">
 <!--Greeting the user-->
-	Hi <?php echo "{$_POST['username']}"; ?>.<br/>
-	Your email: <?php echo "{$_POST['email']}"; ?><br/>
-
-<!--Interact with database-->
 	<?php
 	// access database and posted data
 	$db = new SQLite3('users.db') or die('Unable to open database');
 	$username = $_POST['username'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-
-	// list database content before insertion
-	$result = $db->query('SELECT * FROM users') or die('Query failed');
-	echo "Username\t|\tEmail\t|\tPassword<br/>";
-	while($row = $result->fetchArray())
+	
+	// check if the user already registered
+	$check = "SELECT * FROM users WHERE username = '$username'";
+	$result = $db->query($check);
+	if($row = $result->fetchArray())
 	{
-		echo "{$row['username']}\t|\t{$row['email']}\t|\t{$row['password']}<br/>";
-	}
-
-	// insert the new user
-	$query = <<<EOD
-	INSERT INTO users VALUES ('$username', '$email', '$password')
+		echo "Sorry, but you already exist";
+	} else
+	{
+		// insert the new user
+		$query = <<<EOD
+		INSERT INTO users VALUES ('$username', '$email', '$password')
 EOD;
-	$db->exec($query) or die("Unable to add user $username");
+		$db->exec($query) or die("Unable to add user $username");
 
-	// list database content after insertion
-	echo "<br/>";
-	echo "After insertion:<br/>";
-	echo "Username\t|\tEmail\t|\tPassword<br/>";
-	while($row = $result->fetchArray())
-	{
-		echo "{$row['username']}\t|\t{$row['email']}\t|\t{$row['password']}<br/>";
+		echo "Welcome, $username!";
 	}
+	echo "<br/>";
 	?>
 </body>
 </html>
