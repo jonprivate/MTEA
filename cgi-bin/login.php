6db0264ea94f42c92205e9be0d617db7e8dc0cfd
jonprivate <jonprivate1991@hotmail.com>
2014-10-10 10:34:19 -0600

    separate main web page, register page and login page, add the password input mode

--- cgi-bin/login.php
+++ cgi-bin/login.php
@@ -3,15 +3,14 @@
 	<header class="tophead">
 		<a href="http://jiong-liu.rochestercs.org/index.html">
 		<h1>Mella Tea</h1>
-		<h2>Enjoy a cup of life</h2>
+		<h2>Enjoy a cup of life!</h2>
 		</a>
 	</header>
-
+	
 	<?php
 	// access database and posted data
 	$db = new SQLite3('users.db') or die('Unable to open database');
 	$username = $_POST['username'];
-	$email = $_POST['email'];
 	$password = $_POST['password'];
 	
 	// check if the user already registered
@@ -19,21 +18,16 @@
 	$result = $db->query($check);
 	if($row = $result->fetchArray())
 	{
-		echo "Sorry, but you already exist<br/>";
-		$link = <<<EOD
-		<a href="http://jiong-liu.rochestercs.org/login.html">You can login here</a>
-EOD;
-		echo $link;
+		echo "Hello $username, welcome back!";
 	} else
 	{
-		// insert the new user
-		$query = <<<EOD
-		INSERT INTO users VALUES ('$username', '$email', '$password')
+		echo "Sorry, but you are not registered yet.";
+		echo "<br/>";
+		$link = <<<EOD
+		<a href="http://jiong-liu.rochestercs.org/register.html">You can register here :)</a>
 EOD;
-		$db->exec($query) or die("Unable to add user $username");
-
-		echo "Welcome, $username!";
-	}
+		echo $link;
+	}	
 	echo "<br/>";
 	?>
 </body>
